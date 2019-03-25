f588b4fe60dd79de4982697b7b3c25ad305ea1d7
dongpani <epyruskdh@gmail.com>
2019-03-26 08:23:52 +0900

    가위바위보 게임 마무리

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # zeroCho-Study
-제로초님 강좌기반 자바스크립트 기초 스터디
+- 제로초님 강좌기반 자바스크립트 기초 스터디
+- 유튜브 강좌
 
 ## VSCode 깃허브 레파지토리 복사하기.
 1. ctrl + shift + p
 2. git clone
 3. repository url 입력 후 폴더 지정.
+4. 스테이징 -> 커밋 -> Push
--- 가위바위보/가위바위보.js
+++ 가위바위보/가위바위보.js
@@ -35,101 +35,43 @@ const 게임 = setInterval(function() {
     'url(https://en.pimg.jp/023/182/267/1/23182267.jpg)' + 컴퓨터가내는거 + 'px, 0';
 }, 500);
 
+
+const 쇼부 = {
+    가위 : "1",
+    바위 : "0",
+    보 : "-1"
+}
+
 // 버튼들을 반복하면서 클릭 이벤트를 추가한다.
 document.querySelectorAll('.btn').forEach(function(btn) {
     btn.addEventListener('click', function(e) {
-
-        // console.log(btn[]);
-
-        /*
-        승부를 결정하는 조건문의 코드의 중복을 없애라.
-        힌트 : 딕셔너리, 1, 0, -1
-
-        가위 : 1
-        바위 : 0
-        보 : -1
-
-             이겼을 때    졌을 때     비겼을 때
-    가위 :    1 / -1 =  2      1 / 0  = 1     1 / 1 = 0
-    바위 :    0  / 1  = -1      0 / -1 =  1     0 / 0 = 0
-    보   :   -1 / 0  = -1       -1 / 1 = -2      -1 / -1 = 0
-
-        */
-
-        console.log((-1) - (-1));
-
-        const 쇼부 = {
-            가위 : "1",
-            바위 : "0",
-            보 : "-1"
-        }
 
         let 내손 = this.textContent;
 
         // 이벤트 발생 시, 내가 내는거와 컴퓨터가 내는게 일치하면, 승리를 찍고, 셋인터벌을 멈춤.
         console.log(내손, 컴퓨터의선택(컴퓨터가내는거));
-        
-        // 내가 내는 손 마다 규칙을 찾음.
-        // 이겼을 때, 졌을 떄, 비겼을 때
 
         // 
 
         // 이겼을 떄
-        if(쇼부[내손] - 쇼부[컴퓨터의선택(컴퓨터가내는거)] === 2 ||  쇼부[내손] - 쇼부[컴퓨터의선택(컴퓨터가내는거)] === -1) {
+        if([2, -1].includes(쇼부[내손] - 쇼부[컴퓨터의선택(컴퓨터가내는거)])) {
             console.log('이겨따!!!');
-        } else if(쇼부[내손] - 쇼부[컴퓨터의선택(컴퓨터가내는거)] === 1 ||  쇼부[내손] - 쇼부[컴퓨터의선택(컴퓨터가내는거)] === -2) {  // 졌을 떄
+        } else if([1, -2].includes(쇼부[내손] - 쇼부[컴퓨터의선택(컴퓨터가내는거)])) {  // 졌을 떄
             console.log('졌다...졌다!!!!!');
         } else if(쇼부[내손] - 쇼부[컴퓨터의선택(컴퓨터가내는거)] === 0) {
             console.log('한번 더 하자!!');
         }
-
-        /*
-        if(내손 === '바위') {
-            if(컴퓨터의선택(컴퓨터가내는거) === '가위') { // 이겼을 때              
-                console.log('이겨따!!!');
-                clearInterval(게임);
-                alert('이겼다!!');
-            } else if(컴퓨터의선택(컴퓨터가내는거) === '보') { // 졌을 때
-                console.log('졌다...졌다!!!!!');
-            } else {  // 비겼을 때
-                console.log('한번 더 하자!!');
-            }
-
-        } else if(내손 === '가위') {
-            if(컴퓨터의선택(컴퓨터가내는거) === '보') { // 이겼을 때
-                console.log('이겨따!!!');
-                clearInterval(게임);
-                alert('이겼다!!');
-            } else if(컴퓨터의선택(컴퓨터가내는거) === '바위') { // 졌을 때
-                console.log('졌다...졌다!!!!!');
-            } else {  // 비겼을 때
-                console.log('한번 더 하자!!');
-            }            
-
-        } else if(내손 === '보') {
-            if(컴퓨터의선택(컴퓨터가내는거) === '바위') { // 이겼을 때                
-                console.log('이겨따!!!');
-                clearInterval(게임);
-                alert('이겼다!!');
-            } else if(컴퓨터의선택(컴퓨터가내는거) === '가위') { // 졌을 때
-                console.log('졌다...졌다!!!!!');
-            } else {  // 비겼을 때
-                console.log('한번 더 하자!!');
-            }            
-        }
-        */
 
     });
 });
 
 // 반복을 멈춤.
 
-/*
 document.querySelector('#stop').addEventListener('click', function() {
     // console.log('123123');
     clearInterval(게임);
 });
-*/
+
 
 document.querySelector('#replay').addEventListener('click', function() {
     // console.log('123123');
