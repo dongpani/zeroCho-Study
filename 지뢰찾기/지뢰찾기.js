00bf74ac0c5ff33100a885b128e294369dbe63ab
dongpani <epyruskdh@gmail.com>
2019-03-28 00:03:51 +0900

    지뢰심기

--- 지뢰찾기/지뢰찾기.js
+++ 지뢰찾기/지뢰찾기.js
@@ -5,7 +5,26 @@ document.querySelector('#exec').addEventListener('click', function() {
     var ver = parseInt(document.querySelector('#ver').value); // 세로
     var mine = parseInt(document.querySelector('#mine').value);  // 지뢰
 
-    console.log(`her=${hor}, ver=${ver}, mine=${mine}`);
+
+    var 후보군 = Array(hor*ver).fill().map(function(요소, 인덱스) {
+        return 인덱스 + 1;
+    });
+
+    console.log('후보군', 후보군);
+    
+    // 셔플이라는 변수에 배열을 선언해
+    var 셔플 = [];
+    
+    // 반복문을 돌건데 후보군이 0 보다 클 때니까 무조건 도는 것이야.
+    // 강제로 끊어주지 않으면 크롬 마비됨.
+     while(후보군.length > 80) {
+        var 이동값 = 후보군.splice(Math.floor(Math.random() * 후보군.length), 1)[0];
+        셔플.push(이동값);
+    }
+
+    console.log('셔플', 셔플);
+
+//    console.log(`her=${hor}, ver=${ver}, mine=${mine}`);
 
     var dataset = [];
 
@@ -22,9 +41,20 @@ document.querySelector('#exec').addEventListener('click', function() {
             arr.push(1);
             var td = document.createElement('td');
             tr.append(td);
-            td.textContent = 1;
+            // td.textContent = 1;
         }
         tbody.appendChild(tr);
     }
+
+    // 지뢰 심기 => 20개
+    for(var k=0; k<셔플.length; k++) {
+        var 세로 = Math.floor(셔플[k] / 10);
+        var 가로 = 셔플[k] % 10;
+        console.log(세로, 가로);
+        tbody.children[세로].children[가로].textContent = 'X';
+
+        // console.log(dataset[세로][가로]);
+    }
+
     console.log(dataset);
 });
