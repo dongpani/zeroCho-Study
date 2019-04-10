4d44c303da243c32c167e6416a43017cbc283ca6
dongpani <epyruskdh@gmail.com>
2019-04-11 08:36:37 +0900

    틱택도 고도화

--- 틱택토/틱택토.js
+++ 틱택토/틱택토.js
@@ -26,7 +26,6 @@ var 비동기콜백 = function(이벤트) {
     // 칸이 비어있으면 X 라고 표시해라.
     if(칸들[몇줄][몇칸].textContent === '') {
         칸들[몇줄][몇칸].textContent = 턴;
-       
 
         // 가로줄 검사
         if(칸들[몇줄][0].textContent === 턴 && 칸들[몇줄][1].textContent === 턴 && 칸들[몇줄][2].textContent === 턴) {
@@ -34,7 +33,6 @@ var 비동기콜백 = function(이벤트) {
         }
 
         // 새로줄 검사
-
         if(칸들[0][몇칸].textContent === 턴 && 칸들[1][몇칸].textContent === 턴 && 칸들[2][몇칸].textContent === 턴) {
             승리 = true;
         }        
@@ -48,7 +46,6 @@ var 비동기콜백 = function(이벤트) {
         if(칸들[2][0].textContent === 턴 && 칸들[1][1].textContent === 턴 && 칸들[0][2].textContent === 턴) {
             승리 = true;
         }
-
 
         // 승리했으면 모든 칸의 textContent 를 값을 초기화 하라.
 
@@ -68,9 +65,13 @@ var 비동기콜백 = function(이벤트) {
         } else {
             // 차례가 끝났으면, 다음턴으로 넘긴다.
             (턴 === 'X') ? 턴 = 'O' : 턴 = 'X';
+
+            // 모든 칸을 조회하여 비어있는 칸을 찾는다.
+            // 그 칸의 인덱스를 저장한다.
+            // 저장한 index 를 랜덤으로 돌린다.
+            // 랜덤으로 하나만 뽑아 'O' 를 찍어준다.
+
         }
-
-
 
     } else { 
         // console.log('빈칸이 아닙니다.');
@@ -91,7 +92,6 @@ for(var i=1; i<=3; i++) {
         칸.addEventListener('click', 비동기콜백);
         줄.appendChild(칸);
     }
-
     테이블.appendChild(줄);
 }
 
