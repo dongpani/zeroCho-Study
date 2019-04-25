cd94be84a7cb98149c34b947afc94924872fb6ed
dongpani <epyruskdh@gmail.com>
2019-04-25 21:10:03 +0900

    프로토타입, 생성자

--- 문법 및 이론/프로토타입.js
+++ 문법 및 이론/프로토타입.js
@@ -8,10 +8,9 @@ var 프로토타입 = {
 };
 
 function 카드공장(name, att, hp) {
-    var 카드 = Object.create(프로토타입);  // 객체 참조 복사가 이루어짐.
+    var 카드 = Object.create(프로토타입);  // 객체 참조복사가 이루어짐.
     카드.name = name;
     카드.att = att;
     카드.hp = hp;
+    return 카드;
 }
-
-
