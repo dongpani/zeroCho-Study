b7d26f1f83911c12698b07e48d3976f9d0dc2727
dongpani <epyruskdh@gmail.com>
2019-05-06 00:47:31 +0900

    방향에 따라 숫자 이동하기

--- 2048/2048.js
+++ 2048/2048.js
@@ -23,8 +23,6 @@ function 초기화() {
 function 랜덤생성() {
     var 빈칸배열 = []; // 빈 배열 생성.
 
-    console.log('데이터', 데이터);
-    
     //  console.log(데이터); // [ [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0] ]
     //                               
 
@@ -60,30 +58,112 @@ function 그리기() {
 그리기();
 
 var 드래그시작 = false; // 마우스가 클릭할 때만 움직임을 감지하게끔
+var 드래그중 = false;
 var 시작좌표;
 var 끝좌표;
 
 // 마우스를 누를 때
 window.addEventListener('mousedown', function(이벤트) {
-    console.log('mousedown', 이벤트);
     드래그시작 = true;
     시작좌표 = [이벤트.clientX, 이벤트.clientY]; // 마우스를 누른 시점에 x, y 좌표를 가져옴.
-    console.log(시작좌표);
+    // console.log(시작좌표);
 });
 
 // 마우스 움직일 때
 window.addEventListener('mousemove', function(이벤트) {
     if(드래그시작) {
-        console.log('mousemove', 이벤트);
+        드래그중 = true;
     }
     
 });
 
 // 마우스를 뗄 떄
 window.addEventListener('mouseup', function(이벤트) {
-    console.log('mouseup', 이벤트);
-    드래그시작 = false;
     끝좌표 = [이벤트.clientX, 이벤트.clientY]; // 마우스를 땐 시점의 좌표
-    console.log(끝좌표);
+    x차이 = 끝좌표[0] - 시작좌표[0]; // 끝 x  빼기 시작 x
+    y차이 = 끝좌표[1] - 시작좌표[1]; // 끝 y  빼기 시작 y
+
+    console.log('x차이', x차이, 'y차이', y차이) ;
+
+
+    // Math.abs 함수는 무조건 양수만 반환
+    if(드래그중) {
+        var 방향;
+        if(x차이 < 0 && Math.abs(x차이) / Math.abs(y차이) > 1) {
+            방향 = '왼쪽';
+        } else if (x차이 > 0 && Math.abs(x차이) / Math.abs(y차이) > 1) {
+            방향 = '오른쪽';
+        } else if(y차이 > 0 && Math.abs(x차이) / Math.abs(y차이) < 1) {
+            방향 = '아래';
+        } else if(y차이 < 0 && Math.abs(x차이) / Math.abs(y차이) < 1) {
+            방향 = '위';
+        }
+    }
+
+    드래그시작 = false;
+    드래그중 = false;
+
+    // 드래그 방향에 따라 숫자 이동하기
+    switch (방향) {
+        case '왼쪽':
+            break;
+        case '오른쪽':
+            break;
+        case '위':
+            var 새데이터 = [
+                [],
+                [],
+                [],
+                []
+            ];
+           데이터.forEach(function(열데이터, i) {
+                열데이터.forEach(function(행데이터, j) {
+                    if(행데이터) {
+                        새데이터[j].unshift(행데이터);
+                    }
+                });
+            });
+            
+            console.log('새데이터', 새데이터);
+            
+            // 맨위로 올림
+            [1, 2, 3, 4].forEach(function(행데이터, i) {
+                [1, 2, 3, 4].forEach(function(열데이터, j) {
+                    데이터[j][i] = 새데이터[i][j] || 0; // 값이 없으면 0
+                });
+            });
+
+            break;
+        case '아래':
+            var 새데이터 = [
+                [],
+                [],
+                [],
+                []
+            ];
+            데이터.forEach(function(열데이터, i) {
+                열데이터.forEach(function(행데이터, j) {
+                    if(행데이터) {
+                        새데이터[j].unshift(행데이터);
+                    }
+                });
+            });
+            
+            console.log('새데이터', 새데이터);
+            
+            // 맨아래로 내림
+            [1, 2, 3, 4].forEach(function(행데이터, i) {
+                [1, 2, 3, 4].forEach(function(열데이터, j) {
+                    데이터[3-j][i] = 새데이터[i][j] || 0; // 값이 없으면 0
+                });
+            });
+            
+
+            break;
+    }
+
+    그리기();
+    랜덤생성();
+
 });
 
